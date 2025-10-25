2dcbefbf60ca768137db3a82df70417ca249fcd5
harsha-xilinx <103888958+harsha-xilinx@users.noreply.github.com>
2025-10-25 07:14:10 +0530

    Create sync_fifo_axi5.v

---
+++ sync_fifo_axi5.v
@@ -0,0 +1,5 @@
+/*
+This is slave side AXI5 based synchronous FIFO code
+So, here we are writingbasic code of AXI5 based, and we need to write logic of AXI5 based VALID and READY signals here then
+*/
+
